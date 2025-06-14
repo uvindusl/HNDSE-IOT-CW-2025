f2592e41ec86e2e486d7c5eef2981a31719ba0f3
haribduvihan <harinduvihan@gmail.com>
2025-06-14 23:31:40 +0530

    edit the css of accident-detail-card

--- Smart-Helmet-WebSite/src/Components/AccidentDetailsCard.jsx
+++ Smart-Helmet-WebSite/src/Components/AccidentDetailsCard.jsx
@@ -2,10 +2,14 @@ function AccidentDetailsCard() {
   return (
     <div className="accident-detail-container">
       <div className="accident-card">
-        <span className="accident-txt">Accident Location: </span>
-        <span className="accident-txt">Accident Time: </span>
-        <span className="accident-txt">Heart Rate When Accident happen: </span>
-        <span className="accident-txt">Speed When Accident happen: </span>
+        <span className="accident-txt">Accident Location: </span> <br></br>
+        <span className="accident-txt">Accident Time: </span> <br></br>
+        <span className="accident-txt">
+          Heart Rate When Accident happen:{" "}
+        </span>{" "}
+        <br></br>
+        <span className="accident-txt">Speed When Accident happen: </span>{" "}
+        <br></br>
       </div>
     </div>
   );
--- Smart-Helmet-WebSite/src/Components/HeartBeatLineChart.jsx
+++ Smart-Helmet-WebSite/src/Components/HeartBeatLineChart.jsx
@@ -3,15 +3,17 @@ import { LineChart } from "@mui/x-charts/LineChart";
 
 function HeartBeatLineChart() {
   return (
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      height={300}
-    />
+    <div className="heart-beat-container">
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        height={300}
+      />
+    </div>
   );
 }
 export default HeartBeatLineChart;
