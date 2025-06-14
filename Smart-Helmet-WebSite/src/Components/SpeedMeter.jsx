0d8e6715b7575522d296cbc460990ace77ca92df
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-06-14 23:47:50 +0530

    changes in speed meter css and app.css

--- Smart-Helmet-WebSite/src/Components/SpeedMeter.jsx
+++ Smart-Helmet-WebSite/src/Components/SpeedMeter.jsx
@@ -1,10 +1,30 @@
 import * as React from "react";
-import { Gauge } from "@mui/x-charts/Gauge";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+
+const settings = {
+  width: 100,
+  height: 100,
+  value: 60,
+};
 
 function SpeedMeter() {
   return (
     <div className="speed-meter-container">
-      <Gauge width={100} height={100} value={60} />
+      <Gauge
+        {...settings}
+        cornerRadius="50%"
+        sx={(theme) => ({
+          [`& .${gaugeClasses.valueText}`]: {
+            fontSize: 40,
+          },
+          [`& .${gaugeClasses.valueArc}`]: {
+            fill: "#52b202",
+          },
+          [`& .${gaugeClasses.referenceArc}`]: {
+            fill: theme.palette.text.disabled,
+          },
+        })}
+      />
     </div>
   );
 }
