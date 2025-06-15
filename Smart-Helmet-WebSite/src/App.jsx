30f8af0a529c536baeea729a23fa63be183ebb8b
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-06-15 11:21:21 +0530

    add firebase dependencies and config firebase and get data from firebase

--- Smart-Helmet-WebSite/src/App.jsx
+++ Smart-Helmet-WebSite/src/App.jsx
@@ -5,7 +5,33 @@ import RiderDetailsCard from "./Components/RiderDetailsCard";
 import SpeedMeter from "./Components/SpeedMeter";
 import "./css/App.css";
 
+// testing prepose
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { useState, useEffect } from "react";
+
 function App() {
+  //use State for get data and set data to array
+  const [datas, setData] = useState([]);
+
+  //async function to get data from firebase
+  const fetchPost = async () => {
+    await getDocs(collection(db, "Activation")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      //set data to setData array
+      setData(newData);
+      //print data in console
+      console.log(datas, newData);
+    });
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
   return (
     <>
       <NavBar />
