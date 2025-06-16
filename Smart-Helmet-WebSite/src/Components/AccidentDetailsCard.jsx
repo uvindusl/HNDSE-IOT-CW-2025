1c394a5e9a3bbdee713ed222de0863e8f788a9eb
haribduvihan <harinduvihan@gmail.com>
2025-06-16 21:43:30 +0530

    accident deatils updated

--- Smart-Helmet-WebSite/src/Components/AccidentDetailsCard.jsx
+++ Smart-Helmet-WebSite/src/Components/AccidentDetailsCard.jsx
@@ -4,17 +4,15 @@ function AccidentDetailsCard() {
       <div className="accident-card">
         <span className="accident-txt">Accident Location: </span> <br></br>
         <span className="accident-txt">Accident Time: </span> <br></br>
-        <span className="accident-txt">
-          Heart Rate When Accident happen:{" "}
-        </span>{" "}
-        <br></br>
-        <span className="accident-txt">Speed When Accident happen: </span>{" "}
+        <span className="accident-txt">Date: </span> <br></br>
         <br></br>
       </div>
       <div className="accident-card">
-        <span className="accident-txt">Date: </span> <br></br>
-        <span className="accident-txt">Status: </span> <br></br>
-        <span className="accident-txt">time: </span> <br></br>
+        <span className="accident-txt">Heart Rate When Accident happen: </span>
+        <br></br>
+        <span className="accident-txt">Speed When Accident happen: </span>
+        <br></br>
+        <span className="accident-txt">Deaccelaration Rate: </span> <br></br>
       </div>
     </div>
   );
