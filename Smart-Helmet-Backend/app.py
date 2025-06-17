45dfe56e008d65d3c5a5754f585334b1e2117cbe
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-06-17 21:51:30 +0530

    fix the issue and add another method to get vital data

--- Smart-Helmet-Backend/app.py
+++ Smart-Helmet-Backend/app.py
@@ -17,11 +17,11 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY' , '123')
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 
 #this will be change in future it will come to backend from mobile app or firebase idk
-riderNic =  "0384328472"
+helmetID =  "h0222"
 
 @app.route('/riders', methods=['GET'])
 def getRiderDetails():
-    riderData = db.collection('Riders').where('NIC', '==', riderNic)
+    riderData = db.collection('Riders').where('h_id', '==', helmetID)
     docs = riderData.get()
 
     riderData = []
@@ -31,7 +31,7 @@ def getRiderDetails():
 
 @app.route('/accidents', methods=['GET'])
 def getAccidentDetails():
-    accidentData = db.collection('Accidents').where('NIC', '==', riderNic)
+    accidentData = db.collection('Accidents').where('h_id', '==', helmetID)
     docs = accidentData.get()
 
     accidentData = []
@@ -41,13 +41,23 @@ def getAccidentDetails():
 
 @app.route('/accidentdatas' , methods=['GET'])
 def getAccidentDatas():
-    accidentDatas = db.collection('Accident_Data').where('NIC', '==', riderNic)
+    accidentDatas = db.collection('Accident_Data').where('h_id', '==', helmetID)
     docs = accidentDatas.get()
 
     accidentDatas = []
     for doc in docs:
         accidentDatas.append(doc.to_dict())
     return jsonify(accidentDatas)
+
+@app.route('/vitals' , methods=['GET'])
+def getVitalDetails():
+    vitalData = db.collection('Vitals').where('h_id', '==', helmetID)
+    docs = vitalData.get()
+
+    vitalData = []
+    for doc in docs:
+        vitalData.append(doc.to_dict())
+    return jsonify(vitalData)
 
 if __name__ == '__main__':
     app.run(debug=True , port=5000)
