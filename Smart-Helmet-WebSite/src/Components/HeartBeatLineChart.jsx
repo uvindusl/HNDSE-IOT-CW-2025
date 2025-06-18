3bdb25e705c815ded27da30c45b4e98d64625f44
haribduvihan <harinduvihan@gmail.com>
2025-06-18 20:40:30 +0530

    heart rate

--- Smart-Helmet-WebSite/src/Components/HeartBeatLineChart.jsx
+++ Smart-Helmet-WebSite/src/Components/HeartBeatLineChart.jsx
@@ -1,23 +1,51 @@
 import * as React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
+import { useState, useEffect } from "react";
 
 function HeartBeatLineChart() {
+  const [HeartRate, setHeartRate] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  //function for get data from flask backend
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/vitals");
+        if (!response.ok) {
+          throw new Error(`Heart rate data getting failed`);
+        }
+        const data = await response.json();
+        setHeartRate(data);
+      } catch (error) {
+        setError("Error occured when loading data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(HeartRate);
+
   return (
     <div className="heart-beat-container">
-      <div className="heart-beat-card">
-        <p className="heart-rate-title">Heart-Rate</p>
-        <p className="heart-rate">rate</p>
-        <p className="heart-rate-time">time</p>
-        <LineChart
-          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-          series={[
-            {
-              data: [2, 5.5, 2, 8.5, 1.5, 5],
-            },
-          ]}
-          height={300}
-        />
-      </div>
+      {HeartRate.map((data, index) => (
+        <div className="heart-beat-card" key={index}>
+          <p className="heart-rate-title">Heart-Rate</p>{" "}
+          <p className="heart-rate">rate</p>{" "}
+          <p className="heart-rate-time">time</p>
+          <LineChart
+            xAxis={[{ data: data.heart_beat.map((_, i) => i) }]}
+            series={[
+              {
+                data: data.heart_beat,
+              },
+            ]}
+            height={300}
+          />
+        </div>
+      ))}
     </div>
   );
 }
