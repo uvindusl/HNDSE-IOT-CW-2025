e43660e0b6f7e4a2192c5ec32b907b3e3b096ec6
haribduvihan <harinduvihan@gmail.com>
2025-06-18 20:54:06 +0530

    added loadings

--- Smart-Helmet-WebSite/src/Components/AccelMeter.jsx
+++ Smart-Helmet-WebSite/src/Components/AccelMeter.jsx
@@ -34,6 +34,62 @@ function AccelMeter() {
     fetchData();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="rider-details-container">
+        <div className="rider-card">
+          <svg viewBox="0 0 240 240" height="240" width="240" className="pl">
+            <circle
+              stroke-linecap="round"
+              stroke-dashoffset="-330"
+              stroke-dasharray="0 660"
+              stroke-width="20"
+              stroke="#000"
+              fill="none"
+              r="105"
+              cy="120"
+              cx="120"
+              className="pl__ring pl__ring--a"
+            ></circle>
+            <circle
+              stroke-linecap="round"
+              stroke-dashoffset="-110"
+              stroke-dasharray="0 220"
+              stroke-width="20"
+              stroke="#000"
+              fill="none"
+              r="35"
+              cy="120"
+              cx="120"
+              className="pl__ring pl__ring--b"
+            ></circle>
+            <circle
+              stroke-linecap="round"
+              stroke-dasharray="0 440"
+              stroke-width="20"
+              stroke="#000"
+              fill="none"
+              r="70"
+              cy="120"
+              cx="85"
+              className="pl__ring pl__ring--c"
+            ></circle>
+            <circle
+              stroke-linecap="round"
+              stroke-dasharray="0 440"
+              stroke-width="20"
+              stroke="#000"
+              fill="none"
+              r="70"
+              cy="120"
+              cx="155"
+              className="pl__ring pl__ring--d"
+            ></circle>
+          </svg>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="speed-meter-container">
       {AccidentDetails.map(function (data) {
