6fdfb1acfac1d710bb573ac1216e10631e02da17
haribduvihan <harinduvihan@gmail.com>
2025-06-18 18:16:55 +0530

    get speedometer data

--- Smart-Helmet-WebSite/src/Components/SpeedMeter.jsx
+++ Smart-Helmet-WebSite/src/Components/SpeedMeter.jsx
@@ -1,31 +1,64 @@
 import * as React from "react";
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import { useState, useEffect } from "react";
 
 const settings = {
   width: 100,
   height: 100,
-  value: 60,
+  // value: 70,
 };
 
 function SpeedMeter() {
+  const [AccidentDetails, setAccidentDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // function for get data from flask backend
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/accidents");
+        if (!response.ok) {
+          throw new Error(`Accident data fetching failed`);
+        }
+        const data = await response.json();
+        setAccidentDetails(data);
+      } catch (error) {
+        setError("Error occured when loading data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(AccidentDetails);
+
   return (
     <div className="speed-meter-container">
-      <p className="meter-title">Speed</p>
-      <Gauge
-        {...settings}
-        cornerRadius="50%"
-        sx={(theme) => ({
-          [`& .${gaugeClasses.valueText}`]: {
-            fontSize: 40,
-          },
-          [`& .${gaugeClasses.valueArc}`]: {
-            fill: "#52b202",
-          },
-          [`& .${gaugeClasses.referenceArc}`]: {
-            fill: theme.palette.text.disabled,
-          },
-        })}
-      />
+      {AccidentDetails.map(function (data) {
+        return (
+          <>
+            <p className="meter-title">Speed</p>
+            <Gauge
+              {...settings}
+              value={data.last_speed}
+              cornerRadius="50%"
+              sx={(theme) => ({
+                [`& .${gaugeClasses.valueText}`]: {
+                  fontSize: 40,
+                },
+                [`& .${gaugeClasses.valueArc}`]: {
+                  fill: "#52b202",
+                },
+                [`& .${gaugeClasses.referenceArc}`]: {
+                  fill: theme.palette.text.disabled,
+                },
+              })}
+            />
+          </>
+        );
+      })}
     </div>
   );
 }
