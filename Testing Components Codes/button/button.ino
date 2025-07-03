ef25118883b0a9c30e2c4fd9da561ebfc727c29c
mkdinindu <mkdgangadara0615@gmail.com>
2025-07-03 22:22:56 +0530

    button test code added

---
+++ Testing Components Codes/button/button.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D5, INPUT_PULLUP);
+  pinMode(D2, OUTPUT);
+  Serial.begin(115200);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int state = digitalRead(D5);
+
+  if(state == LOW){
+    Serial.println("Button pressed");
+    digitalWrite(D2, HIGH);
+    
+  }else{
+    Serial.println("Button not pressed");
+    digitalWrite(D2, LOW);
+  }
+  delay(100);
+
+}
