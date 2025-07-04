5e685d437e1d6f2b980dc7fd748b2d7d1393775e
haribduvihan <harinduvihan@gmail.com>
2025-07-04 15:10:59 +0530

    add app name

--- IOT_Android_App/app/src/main/java/com/uhd/helmet/MainActivity.java
+++ IOT_Android_App/app/src/main/java/com/uhd/helmet/MainActivity.java
@@ -90,8 +90,8 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }else{
-                    t2.setText("no data found");
-                    Toast.makeText(this, "no data found", Toast.LENGTH_SHORT).show();
+                    t2.setText("Invalid helmet id");
+                    Toast.makeText(this, "Invalid helmet id", Toast.LENGTH_SHORT).show();
                 }
             }else{
                 t2.setText("error");
